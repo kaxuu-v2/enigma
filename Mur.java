051b8b22458d48fd3016739cc8ae8393d86dd7ff
Kazuu <donovan.vi@universite-paris-saclay.fr>
2025-12-10 22:06:48 +0100

    Update Mur.java

--- Mur.java
+++ Mur.java
@@ -20,7 +20,7 @@ public class Mur extends Square {
     }
 
     /*
-    Note : Beaucoup trop complexe a coder et surtout très difficile a débugger,
+    Note : A priori, touch() beaucoup trop complexe a coder et surtout très difficile a débugger,
     on va subdiviser les cas en des méthodes séparées à la place de faire un seul touch().
     */
 
@@ -46,57 +46,6 @@ public class Mur extends Square {
             b.inverserVy();
         }
     }
-
-    @Override
-    public void touchHorizontal(Ball b){
-        // Appelé pour les murs à Gauche et à Droite
-        if (estEnContact(b)) {
-            b.inverserVx();
-        }
-    }
-
-    @Override
-    public void touchVertical(Ball b){
-        // Appelé pour les murs en Haut et en Bas
-        if (estEnContact(b)) {
-            b.inverserVy();
-        }
-    }
-
-    @Override
-    public void touchCorner(Ball b){
-        // Appelé pour les diagonales
-        if (estEnContact(b)) {
-            // Dans un coin, on inverse tout pour un rebond propre
-            b.inverserVx();
-            b.inverserVy();
-        }
-    }
-
-    // --- MÉTHODE DE DÉTECTION (HITBOX) ---
-    // Vérifie si le rectangle de la balle chevauche le rectangle du mur
-    private boolean estEnContact(Ball b) {
-        // Coordonnées de la Balle (Centre et Rayon)
-        double bx = b.getX();
-        double by = b.getY();
-        double br = Ball.rayon; // Ton rayon est 0.3
-
-        // Coordonnées du Mur (i=Ligne=Y, j=Colonne=X)
-        // Le mur va de j à j+1 en X, et de i à i+1 en Y
-        double murGauche = this.j;
-        double murDroit = this.j + 1;
-        double murHaut = this.i;
-        double murBas = this.i + 1;
-
-        // Test de chevauchement AABB (Axis-Aligned Bounding Box)
-        // Est-ce que la balle touche le mur horizontalement ?
-        boolean toucheX = (bx + br > murGauche) && (bx - br < murDroit);
-        // Est-ce que la balle touche le mur verticalement ?
-        boolean toucheY = (by + br > murHaut) && (by - br < murBas);
-
-        return toucheX && toucheY;
-    }
-
 
     public String toString(){
         return "#";
